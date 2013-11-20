ba2265c730fcf7b3bff2eb5c68c898529699bfc4
nomorecoin <morebitsmorecoin@gmail.com>
2013-11-20 06:01:13 -0600

    Fix decrypt function, respect compressed flagbyte

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 Python BIP0038
 =======
-Python implementation of BIP38 - https://en.bitcoin.it/wiki/BIP_0038  
+Partial Python implementation of BIP38 - https://en.bitcoin.it/wiki/BIP_0038  
+No ec-multiply modes at this time  
 Encrypt function based on modified code from https://github.com/sowbug/cold-wallet-generator  
-Decrypt is incomplete and returns incorrect values  
+Thanks to /u/thatdontmakenosense for finishing the decrypt function  
   
-Goals
-=======
-Focus on non-ec-multiply mode initially  
-Respect flagbytes in all modes  
+
--- bip38.py
+++ bip38.py
@@ -7,11 +7,13 @@ from pybitcointools import *
 import binascii
 import base58
 
-# Encrypt function works as expected.
-# Decrypt does not function.
-# Decrypt steps are verified as far as encryptedhalf1 and encrypted half2
 
-# TODO respect compression flagbyte on decrypt
+# BIP0038 proposal test cases for non-ec multiply mode verified
+# Additional test cases verified with bitaddress.org
+
+# TODO:
+# verify encrypted privkey checksum before decrypting?
+
 
 tests = [{'passphrase':'TestingOneTwoThree',
           'expectedpriv':"6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEbn2Nh2ZoGg",
@@ -28,16 +30,24 @@ compresstests = [{'passphrase':'TestingOneTwoThree',
                   'expectedaddr':"164MQi977u9GUteHr4EPH27VkkdxmfCvGW"},
                  {'passphrase':'Satoshi',
                   'expectedpriv':"6PYLtMnXvfG3oJde97zRyLYFZCYizPU5T3LwgdYJz1fRhh16bU7u6PPmY7",
-                  'expectedwif':"KwYgW8gcxj1JWJXhPSu4Fqwzfhp5Yfi42mdYmMa4XqK7NJxXUSK7 ",
+                  'expectedwif':"KwYgW8gcxj1JWJXhPSu4Fqwzfhp5Yfi42mdYmMa4XqK7NJxXUSK7",
                   'expectedaddr':"1HmPbwsvG5qJ3KJfxzsZRZWhbm1xBMuS8B"}]
 
-def bip38_encrypt(privkey,passphrase,compressed=False):
+def bip38_encrypt(privkey,passphrase):
     '''BIP0038 non-ec-multiply encryption. Returns BIP0038 encrypted privkey.'''
     privformat = get_privkey_format(privkey)
     if privformat in ['wif_compressed','hex_compressed']:
         compressed = True
-    if privformat not in ['hex','hex_compressed']:
+        flagbyte = '\xe0'
+        if privformat == 'wif_compressed':
+            privkey = encode_privkey(privkey,'hex_compressed')
+            privformat = get_privkey_format(privkey)
+    if privformat in ['wif', 'hex']:
+        compressed = False
+        flagbyte = '\xc0'
+    if privformat == 'wif':
         privkey = encode_privkey(privkey,'hex')
+        privformat = get_privkey_format(privkey)
     pubkey = privtopub(privkey)
     addr = pubtoaddr(pubkey)
     addresshash = hashlib.sha256(hashlib.sha256(addr).digest()).digest()[0:4]
@@ -46,14 +56,9 @@ def bip38_encrypt(privkey,passphrase,compressed=False):
     derivedhalf2 = key[32:64]
     aes = AES.new(derivedhalf2)
     encryptedhalf1 = aes.encrypt(binascii.unhexlify('%0.32x' % (long(privkey[0:32], 16) ^ long(binascii.hexlify(derivedhalf1[0:16]), 16))))
-    aes = AES.new(derivedhalf2)
     encryptedhalf2 = aes.encrypt(binascii.unhexlify('%0.32x' % (long(privkey[32:64], 16) ^ long(binascii.hexlify(derivedhalf1[16:32]), 16))))
-    if compressed:
-        flagbyte = '\xe0'
-    else:
-        flagbyte = '\xc0'
     encrypted_privkey = ('\x01\x42' + flagbyte + addresshash + encryptedhalf1 + encryptedhalf2)
-    encrypted_privkey += hashlib.sha256(hashlib.sha256(encrypted_privkey).digest()).digest()[:4]
+    encrypted_privkey += hashlib.sha256(hashlib.sha256(encrypted_privkey).digest()).digest()[:4] # b58check for encrypted privkey
     encrypted_privkey = base58.b58encode(encrypted_privkey)
     return encrypted_privkey
 
@@ -62,13 +67,13 @@ def bip38_decrypt(encrypted_privkey,passphrase):
     d = base58.b58decode(encrypted_privkey)
     d = d[2:]
     flagbyte = d[0:1]
+    d = d[1:]
     if flagbyte == '\xc0':
         compressed = False
     if flagbyte == '\xe0':
         compressed = True
-    d = d[1:]
     addresshash = d[0:4]
-    d = d[4:-4] # last 4 = b58 checksum?
+    d = d[4:-4]
     key = scrypt.hash(passphrase,addresshash, 16384, 8, 8)
     derivedhalf1 = key[0:32]
     derivedhalf2 = key[32:64]
@@ -78,18 +83,16 @@ def bip38_decrypt(encrypted_privkey,passphrase):
     decryptedhalf2 = aes.decrypt(encryptedhalf2)
     decryptedhalf1 = aes.decrypt(encryptedhalf1)
     priv = decryptedhalf1 + decryptedhalf2
-    priv = priv.encode('hex')
-    if compressed:
-        priv = encode_privkey(priv,'hex_compressed')
+    priv = binascii.unhexlify('%064x' % (long(binascii.hexlify(priv), 16) ^ long(binascii.hexlify(derivedhalf1), 16)))
     pub = privtopub(priv)
-    addr = pubtoaddr(pub)
-    if hashlib.sha256(hashlib.sha256(addr).digest()).digest()[0:4] == addresshash:
-        print('Address verified.')
+    if compressed:
+        pub = encode_pubkey(pub,'hex_compressed')
+        wif = encode_privkey(priv,'wif_compressed')
     else:
-        print('Address verification failed!')
-    wif = encode_privkey(priv,'wif')
-    #print(addr)
-    #print(wif)
+        wif = encode_privkey(priv,'wif')
+    addr = pubtoaddr(pub)
+    if hashlib.sha256(hashlib.sha256(addr).digest()).digest()[0:4] != addresshash:
+        print('Addresshash verification failed! Password is likely incorrect.')
     return wif
 
 def runtests():
@@ -98,6 +101,7 @@ def runtests():
         expectedpriv = test.get('expectedpriv')
         expectedwif = test.get('expectedwif')
         expectedaddr = test.get('expectedaddr')
+        print('Testing %s' %(expectedwif))
         resultpriv = bip38_encrypt(expectedwif,passphrase)
         if resultpriv == expectedpriv:
             print('Encryption Success!')
@@ -112,20 +116,19 @@ def compresstest():
         expectedpriv = test.get('expectedpriv')
         expectedwif = test.get('expectedwif')
         expectedaddr = test.get('expectedaddr')
-        resultpriv = bip38_encrypt(expectedwif,passphrase,True)
+        print('Testing %s' %(expectedwif))
+        resultpriv = bip38_encrypt(expectedwif,passphrase)
         if resultpriv == expectedpriv:
             print('Encryption Success!')
+        else:
+            print('Encryption Failed!')
+            print('Expected %s' %(expectedpriv))
+            print('Returned %s' %(resultpriv))
         decryptedpriv = bip38_decrypt(resultpriv,passphrase)
         if decryptedpriv == expectedwif:
             print('Decryption Success!')
+        else:
+            print('Decryption Failed!')
+            print('Expected %s' %(expectedwif))
+            print('Returned %s' %(decryptedpriv))
         print('-')*80
-            
-#encrypted_privkey = bip38_encrypt('5KN7MzqK5wt2TP1fQCYyHBtDrXdJuXbUzm4A9rKAteGu3Qi5CVR','TestingOneTwoThree')
-#print(encrypted_privkey)
-
-#decrypted_wif = bip38_decrypt(encrypted_privkey,'TestingOneTwoThree')
-#print(decrypted_wif)
-
-runtests()
-
-
